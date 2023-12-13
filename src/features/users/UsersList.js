aa30ef568cd27edfbe4884b6fc3425c20718eb8a
bwarszynski <bwarszynski@windowslive.com>
2023-12-14 00:35:46 +0100

    Redux Added

--- src/features/users/UsersList.js
+++ src/features/users/UsersList.js
@@ -1,6 +1,48 @@
+import { useGetUsersQuery } from "./usersApiSlice"
+import User from './User'
+
 const UsersList = () => {
-    return (
-        <h1>Lista Użytkowników</h1>
-    )
+
+    const {
+        data: users,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useGetUsersQuery()
+
+    let content
+
+    if (isLoading) content = <p>Ładowanie...</p>
+
+    if (isError) {
+        content = <p className="errmsg">{error?.data?.message}</p>
+    }
+
+    if (isSuccess) {
+
+        const { ids } = users
+
+        const tableContent = ids?.length
+            ? ids.map(userId => <User key={userId} userId={userId} />)
+            : null
+
+        content = (
+            <table className="table table--users">
+                <thead className="table__thead">
+                <tr>
+                    <th scope="col" className="table__th user__username">Nazwa użytkownika</th>
+                    <th scope="col" className="table__th user__roles">Role</th>
+                    <th scope="col" className="table__th user__edit">Edytuj</th>
+                </tr>
+                </thead>
+                <tbody>
+                {tableContent}
+                </tbody>
+            </table>
+        )
+    }
+
+    return content
 }
 export default UsersList
